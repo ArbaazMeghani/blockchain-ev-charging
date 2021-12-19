3d0139160de8d00dc7965d94e8499d8f1d842f56
Arbaaz Meghani <arbaazmeghani@gmail.com>
2021-12-18 19:18:08 -0600

    switch to jit mode to prepare for tailwindcss upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./icons/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
     extend: {},
   },
