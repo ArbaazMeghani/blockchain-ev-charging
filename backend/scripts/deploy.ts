39aacc178b3df2b16a2cb567b6a94213f4a5fc82
Arbaaz Meghani <arbaazmeghani@gmail.com>
2022-01-03 18:27:45 -0600

    deploy contract

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -2,12 +2,12 @@ import { ethers } from "hardhat";
 import fs from "fs";
 
 async function main() {
-  const MyContract = await ethers.getContractFactory("Greeter");
-  const contract = await MyContract.deploy("Hello, Hardhat!");
+  const MyContract = await ethers.getContractFactory("Stations");
+  const contract = await MyContract.deploy();
 
   await contract.deployed();
 
-  console.log("Greeter deployed to:", contract.address);
+  console.log("Stations deployed to:", contract.address);
   const deployment = {
     address: contract.address,
     abi: contract.interface,
