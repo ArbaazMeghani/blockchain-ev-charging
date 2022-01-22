0163182c08dc28d770d53144fd57f5a5fb103a40
Arbaaz Meghani <arbaazmeghani@gmail.com>
2022-01-22 17:09:56 -0600

    add prop types for primary button

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const PrimaryButton = ({ onClick = () => {}, value, type = "button" }) => {
+type Props = {
+  onClick?: () => void;
+  value: string;
+  type?: "submit" | "button";
+};
+
+const PrimaryButton = ({ onClick = () => {}, value, type }: Props) => {
   return (
     <button
       className="p-2 rounded-xl bg-violet-700 mb-8 hover:bg-violet-800 transition-colors duration-300 mr-4"
